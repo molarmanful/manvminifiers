595eb16937a76df7d04a5718723b5d78f2b7c510
Benjamin Pang <molarmanful@users.noreply.github.com>
2015-05-29 09:58:48 -0700

    Delete preminjscrush.js

--- preminjscrush.js
+++
@@ -1,1 +0,0 @@
-_="function c(d){var l/4+1)*14,o='',a=[],b=e=g=0,c=l,f=1e4;a^l;a.push(0))(b=c-=14){--bd*b+(=0?2e3:)*f,g=b+b-1,%g+g)%g,0|d/g;0|e+d/f,o+=h,h,e%f+f)%f}return o}for(;.length=(dd=h=>0;)a[b]";for(Y in $="")with(_.split($[Y]))_=join(pop());eval(_)
