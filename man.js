6e2f7e7400a670242c223efdf060c0994574feca
Benjamin Pang <molarmanful@users.noreply.github.com>
2015-05-29 09:31:32 -0700

    Update man.js

--- man.js
+++ man.js
@@ -1,1 +1,31 @@
-
+function c(digits){
+    var length = (digits / 4 + 1) * 14,
+        out = "",
+        a = [],
+        b = 0,
+        c = length,
+        d = 0,
+        e = 0,
+        f = 10000,
+        g = 0,
+        h = 0;
+    for(;a.length !== length;a.push(0));
+    for(;(b = c -= 14) > 0;){
+        for(;--b > 0;){
+            d *= b;
+            if(h === 0){
+                d += 2000 * f;
+            } else {
+                d += a[b] * f;
+            }
+            g = b + b - 1;
+            a[b] = ((d % g) + g) % g;
+            d = Math.floor(d / g);
+        }
+        h = Math.floor(e + d / f);
+        out += h;
+        h = h.length;
+        d = e = ((d % f) + f) % f;
+    }
+    return out;
+}
