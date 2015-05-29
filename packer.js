2289ed0e36640e85c2c9de2cac2cfb19d8cf8501
Benjamin Pang <molarmanful@users.noreply.github.com>
2015-05-29 09:40:23 -0700

    Update packer.js

--- packer.js
+++ packer.js
@@ -1,1 +1,1 @@
-
+function c(i){var j=(i/4+1)*14,out="",a=[],b=0,c=j,d=0,e=0,f=10000,g=0,h=0;for(;a.length!==j;a.push(0));for(;(b=c-=14)>0;){for(;--b>0;){d*=b;if(h===0){d+=2000*f}else{d+=a[b]*f}g=b+b-1;a[b]=((d%g)+g)%g;d=Math.floor(d/g)}h=Math.floor(e+d/f);out+=h;h=h.length;d=e=((d%f)+f)%f}return out}
