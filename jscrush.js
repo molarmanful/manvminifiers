fe2b67a07dbc06767e4ad88f99a8a721a44c96f2
Benjamin Pang <molarmanful@users.noreply.github.com>
2015-05-29 09:39:58 -0700

    Update jscrush.js

--- jscrush.js
+++ jscrush.js
@@ -1,1 +1,1 @@
-
+_='function c){var  / 41) * 14,"",a[],bc,def10000,gh0;a. !=;a.push(0));(bc -14)--b*b;if(h ==0)2000 *	 else  *	gbb - 1;g)g) % g;d/ g);}he/ f); +h;hh.;def)f) %	return ;} lengthMath.floor( + for(;= d 0,	 f;}(digits > 0;){((% outa[b]{+';for(Y in $='	')with(_.split($[Y]))_=join(pop());eval(_)
